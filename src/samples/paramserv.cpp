830683d9c9931027fc1986acf66fb3a131c54684
TinyOh <tinysilk@hotmail.com>
2021-03-03 02:36:56 +0800

    fix(complie): m1 apple mac log output.

--- src/samples/paramserv.cpp
+++ src/samples/paramserv.cpp
@@ -1,4 +1,6 @@
 #include "parameterserver.h"
+#include <thread>
+#include <chrono>
 
 #define ELPP_THREAD_SAFE
 #define ELPP_FORCE_USE_STD_THREAD
@@ -10,7 +12,9 @@ int main(int argc, char* argv[]) {
   START_EASYLOGGINGPP(argc, argv);
   auto ctrl = ParameterServer::instance()->GetCfgCtrlRoot();
   ctrl["status"] = "ok";
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   LOG(INFO) << "Press Enter to exit." << std::endl;
   std::cin.get();
+  LOG(INFO) << "Exit." << std::endl;
   return 0;
 }
